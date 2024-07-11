66e8daa012ac5ab24bd0c2011231d20d45cf317d
Karol Jajko <karoljajko@gmail.com>
2024-07-11 22:06:13 +0200

    again to many changes to write here + added footer

--- src/js/main.js
+++ src/js/main.js
@@ -7,48 +7,60 @@ const aboutUsBox = document.querySelector('.header-box-elements')
 const aboutUsText = document.querySelector('.header-box-elements-text')
 const btnSkillMap = document.querySelector('.map-skills-box-btn')
 const sillMap = document.querySelector('.map-skills-timeline')
+const btnListSkills = document.querySelector('.list-group-btn')
+const btnListIcon = document.querySelector('.list-group-btn i')
+const listGroupList = document.querySelector('.list-group-next')
 
 const closeNavbar = () => {
-    if(nav.classList.contains('show')){
-        nav.classList.remove('show')
-    }
+	if (nav.classList.contains('show')) {
+		nav.classList.remove('show')
+	}
 }
 
 document.addEventListener('click', closeNavbar)
 
 const openAboutMeSection = () => {
-    // schowaj pasek otweirania aboutUs header
-    btnOpen.classList.add('display--hide')
-    // zmniejszenie img header
-    headerImg.classList.add('header-box-img--close')
-    // Powiększenie box aboutUs header
-    aboutUsBox.classList.add('header-box-elements--open')
-    // nadanie display na tekst
-    aboutUsText.classList.add('display--show')
-    btnCloseHeader.classList.add('display--show')
-
+	// schowaj pasek otweirania aboutUs header
+	// btnOpen.classList.add('header--hide')
+	// zmniejszenie img header
+	headerImg.classList.add('header-box-img--close')
+	// Powiększenie box aboutUs header
+	aboutUsBox.classList.add('header-box-elements--open')
+	// nadanie display na tekst
+	aboutUsText.classList.add('header--show')
+	btnCloseHeader.classList.add('header--show')
 }
 
 const closeAboutMeSection = () => {
-        btnOpen.classList.remove('display--hide')
-        headerImg.classList.remove('header-box-img--close')
-        aboutUsBox.classList.remove('header-box-elements--open')
-        aboutUsText.classList.remove('display--show')
-        btnCloseHeader.classList.remove('display--show')
+	// btnOpen.classList.remove('header--hide')
+	headerImg.classList.remove('header-box-img--close')
+	aboutUsBox.classList.remove('header-box-elements--open')
+	aboutUsText.classList.remove('header--show')
+	btnCloseHeader.classList.remove('header--show')
 }
 
 const openSkillMap = () => {
-    const checkMap = sillMap.classList.contains('display-timeline--show')
-    sillMap.classList.toggle('display-timeline--show')
+	const checkMap = sillMap.classList.contains('display-timeline--show')
+	sillMap.classList.toggle('display-timeline--show')
 
-    if(checkMap == true) {
-         btnSkillMap.innerText = 'pokaż'
-    } else {
-        btnSkillMap.innerText = 'ukryj'
-    } 
+	if (checkMap == true) {
+		btnSkillMap.innerText = 'pokaż'
+	} else {
+		btnSkillMap.innerText = 'ukryj'
+	}
+}
+
+const openListSkill = () => {
+	if (!listGroupList.classList.contains('list-group-next-disply')) {
+		btnListSkills.innerHTML = `Pokaż mniej <i class="bi bi-caret-up"></i>`
+        listGroupList.classList.add('list-group-next-disply')
+	} else {
+        btnListSkills.innerHTML = `Pokaż więcej <i class="bi bi-caret-down"></i>`
+        listGroupList.classList.remove('list-group-next-disply')
+    }
 }
 
 btnOpenHeader.addEventListener('click', openAboutMeSection)
 btnCloseHeader.addEventListener('click', closeAboutMeSection)
 btnSkillMap.addEventListener('click', openSkillMap)
-
+btnListSkills.addEventListener('click', openListSkill)
